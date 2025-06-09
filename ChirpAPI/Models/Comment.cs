ec8cb5195afb6c2b9af853cf40655524eddd9337
MaNnU <mannuspamm@gmail.com>
2025-06-09 12:56:32 +0200

    Add project files.

---
+++ ChirpAPI/Models/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChirpAPI.Models;
+
+public partial class Comment
+{
+    public int Id { get; set; }
+
+    public int ChirpId { get; set; }
+
+    public int? ParentId { get; set; }
+
+    public string Text { get; set; } = null!;
+
+    public TimeOnly CreationTime { get; set; }
+
+    public virtual Chirp Chirp { get; set; } = null!;
+
+    public virtual ICollection<Comment> InverseParent { get; set; } = new List<Comment>();
+
+    public virtual Comment? Parent { get; set; }
+}
